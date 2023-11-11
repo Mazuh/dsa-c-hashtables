6121715c7fdb7c6231fe9c558ea996940e8bc168
mazuh <marcell-mz@hotmail.com>
2023-11-11 17:19:27 -0300

    todos

--- src/hashtables.c
+++ src/hashtables.c
@@ -2,6 +2,18 @@
 
 int main()
 {
+    // TODO: hash of some given values
+
+    // TODO: create set
+
+    // TODO: add some values to set
+
+    // TODO: remove a value from set
+
+    // TODO: try to add a duplicated value to set
+
+    // TODO: print set values
+
     printf("Hello, World!\n");
     return 0;
 }
