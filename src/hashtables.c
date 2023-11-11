bac978ea0eda2266f3da70f11fbf6ae583ac0a54
mazuh <marcell-mz@hotmail.com>
2023-11-11 18:17:25 -0300

    hash djb2

--- src/hashtables.c
+++ src/hashtables.c
@@ -1,8 +1,36 @@
 #include <stdio.h>
 
+/**
+ * @brief Calculate hash value of a raw string using djb2 algorithm.
+ * @see https://theartincode.stanis.me/008-djb2/
+ */
+unsigned long hash_str_djb2(char *raw_string)
+{
+    unsigned long hash_value = 5381;
+
+    int current_char;
+    while ((current_char = *raw_string++))
+    {
+        hash_value = ((hash_value << 5) + hash_value) + current_char;
+    }
+
+    return hash_value;
+};
+
+/**
+ * @brief Experimental execution.
+ */
 int main()
 {
     // TODO: hash of some given values
+    char *str_0 = "";
+    char *str_1 = "Hello, world!";
+    char *str_2 = "Mazuh";
+
+    printf("Hash of '%s': %lu\n", str_0, hash_str_djb2(str_0));
+    printf("Hash of '%s': %lu\n", str_1, hash_str_djb2(str_1));
+    printf("Hash of '%s': %lu\n", str_1, hash_str_djb2(str_1));
+    printf("Hash of '%s': %lu\n", str_2, hash_str_djb2(str_2));
 
     // TODO: create set
 
